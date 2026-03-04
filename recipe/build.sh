03d414ab1d27ba4ac32ba33f364abd785c47b053
Ryan Volz <ryan.volz@gmail.com>
2026-03-04 17:26:49 -0500

    Add CMAKE_POLICY_VERSION_MINIMUM=3.5 for cmake 4 compat

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,11 @@ set -ex
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} -G "Ninja" -DCMAKE_BUILD_TYPE=Release ..
+cmake_config_args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+)
+
+cmake ${CMAKE_ARGS} -G "Ninja" "${cmake_config_args[@]}" ..
 cmake --build . --config Release -- -j${CPU_COUNT}
 cmake --build . --config Release --target install
